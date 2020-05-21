285d1f46853b05515e4f7ceb048823c42fd4d859
studioj <neefsj@gmail.com>
2020-05-22 00:48:38 +0200

    - adding the genea_viewer app which will be a simple view of the database - creating a person db model with a name - setting the urls right

--- gierle_genea_web/test_something.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-from gierle_genea_web import urls
-
-
-class SomeTestCase(TestCase):
-    def test_something(self):
-        self.assertEquals(len(urls.urlpatterns), 1)
