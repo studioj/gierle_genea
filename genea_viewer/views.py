3fe00063205efe5695fc02bac6d964902d1c3fa3
studioj <neefsj@gmail.com>
2020-08-12 00:10:04 +0200

    added a page where in the future you would be able to add a person

--- genea_viewer/views.py
+++ genea_viewer/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the genea_gierle index.")
+
+
+def add_person(request):
+    return HttpResponse("Hello, add your person here")
--- gierle_genea_web/urls.py
+++ gierle_genea_web/urls.py
@@ -17,4 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), url("", include("genea_viewer.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    url("", include("genea_viewer.urls"))
+]
