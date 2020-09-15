cd94fa3913638552364f415e7d3d2cf0bd438b80
Jozefneefs <64045621+Jozefneefs@users.noreply.github.com>
2020-09-15 23:16:49 +0200

    Add person page (#7)
    * added a page where in the future you would be able to add a person
    
    * now we can add a person to the database from the home page
    
    Co-authored-by: studioj <neefsj@gmail.com>

--- genea_viewer/tests.py
+++ genea_viewer/tests.py
@@ -3,13 +3,17 @@ from django.test import TestCase
 from django.urls import resolve
 
 from genea_viewer.models import Person
-from genea_viewer.views import index
+from genea_viewer.views import index, add_person
 
 
-class HomePageTest(TestCase):
+class TestAllURLs(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, index)
+
+    def test_add_person_url_resolves_to_add_person_page_view(self):
+        found = resolve("/add_person")
+        self.assertEqual(found.func, add_person)
 
 
 class PersonModelTestCase(TestCase):
--- genea_viewer/views.py
+++ genea_viewer/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from genea_viewer.models import Person
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the genea_gierle index.")
+    return render(request, 'home.html')
+
+
+def add_person(request):
+    person_ = Person.objects.create(name=request.POST['person_name'])
+    all_persons = Person.objects.all()
+    return render(request, 'home.html', {"added_person": person_.name, "all_persons": all_persons})
