805a27a46e9311b1e7a90bbe1b667a84201cfa73
Neefs, Jef <jef.neefs@barco.com>
2020-11-13 23:56:07 +0100

    first name added

--- genea_viewer/views.py
+++ genea_viewer/views.py
@@ -5,10 +5,12 @@ from genea_viewer.models import Person
 
 
 def index(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
 
 def add_person(request):
-    person_ = Person.objects.create(name=request.POST['person_name'])
+    person_ = Person.objects.create(name=request.POST["person_name"], first_name=request.POST["person_first_name"])
     all_persons = Person.objects.all()
-    return render(request, 'home.html', {"added_person": person_.name, "all_persons": all_persons})
+    return render(
+        request, "home.html", {"added_person_name": person_.name, "added_person_first_name": person_.first_name, "all_persons": all_persons}
+    )
