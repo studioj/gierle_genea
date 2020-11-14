02e6b39ecf4fcb264b33bef6ee5f2ec4e632210c
studioj <neefsj@gmail.com>
2020-11-14 02:38:35 +0100

    Life event birth event  (#9)
    * adding life event
    and birth life event
    
    * adding life event
    and birth life event
    adding a birth
    
    Co-authored-by: Neefs, Jef <jef.neefs@barco.com>

--- genea_viewer/models.py
+++ genea_viewer/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+import datetime
+from pytz import timezone
+
+
+class LifeEvent(models.Model):
+    pass
+
+
+class BirthEvent(LifeEvent):
+    date = models.DateTimeField(null=True)
 
 
 class Person(models.Model):
     name = models.CharField(max_length=200)
     first_name = models.CharField(max_length=200)
+    birth_event = models.ForeignKey(BirthEvent, models.DO_NOTHING, default=1)
